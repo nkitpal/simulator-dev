dee59ad4ffeda934d9a77ddbe9db8941fadb5732
ankitpal <ankit.pal@newtonschool.co>
2024-11-25 11:45:14 +0530

    added frontend auth redirect

--- backend/utils/googleConfig.js
+++ backend/utils/googleConfig.js
@@ -4,9 +4,11 @@ config();
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const YOUR_REDIRECT_URL = process.env.YOUR_REDIRECT_URL;
 
 export const googleConfig = new google.auth.OAuth2(
   GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET,
+  YOUR_REDIRECT_URL,
   "postmessage"
 );
